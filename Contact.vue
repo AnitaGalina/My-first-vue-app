6c16c0f9923630aacf35e6fbe1c30d8fc1352c24
AnitaGalina <anitawanjiku21@gmail.com>
2022-11-17 19:22:01 +0300

    First Commit

---
+++ Contact.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="contact">
+
+        <div id="id">
+            <h2>
+                Contact us
+            </h2>
+            <p> Contact us via our email address at  1234 , Geneva.</p>
+
+        </div>
+        <footer>
+
+          </footer> </div>
+
+    
+</template>
+
+<script>
+    export default {
+        name: 'Contact',
+        data() {
+            return{
+                phoneNumber: '0100-0100-0100'
+            }
+        }
+    }
+</script>
+
+<style scooped="">
+#contact{
+    font-family:-apple system, BlinkMacSystemFont, 'Segoe UI' , Roboto , Oxygen , Ubuntu , Cantarell , 'Open Sans' , 'Helvetica Neue' , sans-serif;
+    padding: 2rem;
+    margin: 2rem;
+    border:1.5 px solid;
+    border-radius: 10px;
+}
+h2{
+    margin: 0.0.1.5rem 0;
+}
+
+
+
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  </div>
+    
+  
+  <router-view>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  </router-view></template>
 
 <style>
 #app {
@@ -21,6 +17,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
